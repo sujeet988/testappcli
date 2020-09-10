7df0e2c58619a912bb0e39ef6bb8090bdb8fb13f
sujeet kumar <sujeetkumar2010@live.com>
2020-09-10 10:57:26 +0530

    testr

--- testappcli/Controllers/WeatherForecastController.cs
+++ testappcli/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace testappcli.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "sujeet", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
